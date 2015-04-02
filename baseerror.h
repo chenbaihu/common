ab835c09ce9a237257b2a75c6e14c6413d45a1f3
chenbaihu.build17 <xychenbaihu@163.com>
2015-04-02 11:55:40 +0800

    add baseerror.h

---
+++ baseerror.h
@@ -0,0 +1,24 @@
+#ifndef _baseerror_H
+#define _baseerror_H
+
+#include <string.h>
+
+class CBaseError
+{
+public:
+	CBaseError()
+	{
+		bzero(m_errbuf,sizeof(m_errbuf));
+	}
+
+	const char * getErr() const  { return m_errbuf;  }
+
+	int setError(const char * fmt,...);
+
+protected:
+	char m_errbuf[1024];
+};
+
+
+#endif
+
